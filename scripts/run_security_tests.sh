d33917019a8ae0c056da0fca1734e36d4752e4ef
Omer Levi Hevroni <omerlh@gmail.com>
2018-07-24 13:03:54 +0300

    update Glue image tag

--- scripts/run_security_tests.sh
+++ scripts/run_security_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $(pwd)/glue:/input soluto/glue-ci:1524727926467 sh -x /app/run_glue.sh /input/glue.json /input/report.json
+docker run --rm -v $(pwd)/glue:/input soluto/glue-ci:1532426297485 sh -x /app/run_glue.sh /input/glue.json /input/report.json
